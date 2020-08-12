0cf64fb879d5f35a13ac326126d22c6814e7c0ec
Javier López <graulopezjavier@gmail.com>
2020-08-12 16:40:15 -0500

    fix: Implement GET instead of POST.
    Odoo by default when It renders the button it always sends a POST
    request, wompi wants a GET, now it checks if the payment acquirer
    selected is Wompi, if so, sets the request action to be GET.

--- __manifest__.py
+++ __manifest__.py
@@ -11,6 +11,7 @@
     'data': [
         'views/payment_views.xml',
         'views/payment_wompicol_templates.xml',
+        'views/template_modify.xml',
         'data/payment_acquirer_data.xml',
     ],
     'post_init_hook': 'create_missing_journal_for_acquirers',
